5725cc1093b3188615f6cd6f45072be0f3566d40
currobellas <curro@uvigo.es>
2018-05-20 22:08:10 +0200

    Commit inicial

---
+++ ServidorMultihiloCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServidorMultihiloCsharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
