3fcb079198855e5c14d63e8f23b7c03ddb7acf09
Curro <curro@colegiovivas.com>
2018-06-26 11:41:46 +0200

    Inicio Servidor

--- ServidorMultihiloCsharp/Program.cs
+++ ServidorMultihiloCsharp/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
 
 namespace ServidorMultihiloCsharp
 {
-    class MainClass
+    public class Servidor
     {
-        public static void Main(string[] args)
+        private const int PUERTO = 31416;
+        public bool Funcionando { set; get; }
+
+        public void InicioServidor()
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Servidor");
+            IPEndPoint ie = new IPEndPoint(IPAddress.Any, PUERTO);
+            Socket socketEscucha = new Socket(AddressFamily.InterNetwork, 
+                                              SocketType.Stream, 
+                                              ProtocolType.Tcp);
+            socketEscucha.Bind(ie); //OJO, si el puerto de escucha 
+                                    //está ocupado lanza excepción.
+            socketEscucha.Listen(5);
+            Console.WriteLine("A la espera");
+
+            while (Funcionando)
+            {
+                Socket socketCliente = socketEscucha.Accept();
+                Thread hiloCliente = new Thread(AtencionACliente);
+                hiloCliente.Start(socketCliente);
+            }
+        }
+
+        public void AtencionACliente(object o)
+        {
+
         }
     }
 }
